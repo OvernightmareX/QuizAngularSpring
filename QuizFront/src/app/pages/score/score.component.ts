a743b16412f350a24537d4373f8692b729b35bdd
VI Loic <vaninghelandtloic@gmail.com>
2024-09-11 16:35:22 +0200

    push pour css

--- QuizFront/src/app/pages/score/score.component.ts
+++ QuizFront/src/app/pages/score/score.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {QuizService} from "../../utils/services/quiz.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-score',
@@ -13,11 +14,15 @@ export class ScoreComponent implements OnInit{
   score: number = 0;
   totalQuestions: number = 0;
 
-  constructor(private quizService: QuizService) {
+  constructor(private quizService: QuizService, private router: Router) {
   }
 
   ngOnInit() {
     this.score = this.quizService.scoreTotal;
     this.totalQuestions = this.quizService.questions.length;
+  }
+
+  goHome() {
+    this.router.navigate(['/']);
   }
 }
